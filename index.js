7840dc58ccbd742a51d084870032bb17d3df308a
Kalpana Swaminathan <kalpana294@gmail.com>
2018-01-10 18:57:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,6 +66,6 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(!cardNumber){
-    console.log(`)
+    console.log(`Sorry, we don't have a credit card on file for you.`);
   }
 }
