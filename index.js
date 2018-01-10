33a5780c35fa20d87e40f7c517b21d02bb8b3a71
Kalpana Swaminathan <kalpana294@gmail.com>
2018-01-10 18:59:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,6 +68,6 @@ function placeOrder(cardNumber) {
   if(!cardNumber){
     console.log(`Sorry, we don't have a credit card on file for you.`);
   }else{
-    console.log(`Your total cost is )
+    console.log(`Your total cost is $${total()}, which will be charged to your ${cardnumber}.`)
   }
 }
