d5a7a916a43cbc484452226b7e91cc3c258573a6
Kalpana Swaminathan <kalpana294@gmail.com>
2018-01-10 19:03:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,9 +69,9 @@ function placeOrder(cardNumber) {
     console.log(`Your total cost is $${total()}, which will be charged to the ${cardNumber}.`);
     for(var i = cart.length;i<=0;i--){
       cart.pop();
-    console.log(`Sorry, we don't have a credit card on file for you.`);
-  }else{
     
+  }else{
+    console.log(`Sorry, we don't have a credit card on file for you.`);
     }
   }
 }
